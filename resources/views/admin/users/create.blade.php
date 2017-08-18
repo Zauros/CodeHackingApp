50a574c4511ce5b5bc17ca50fc642578cff7648d
Michele Oss <mick0@hotmail.com>
2017-08-18 16:25:51 +0200

    Populate the admin area add create and the index

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,34 @@
+@extends("layouts.admin")
+@section("content")
+<h1>Create User</h1>
+
+{!! Form::open(["method" => "POST","action" =>"AdminUsersController@store"]) !!}
+    <div class="form-group">
+    {!! Form::label("name","Name: ") !!}
+    {!! Form::text("name",null,["class"=>"form-control"]) !!}
+    </div>
+<div class="form-group">
+{!! Form::label("email","Email: ") !!}
+{!! Form::email("email",null,["class"=>"form-control"]) !!}
+</div>
+<div class="form-group">
+{!! Form::label("role_is","Role: ") !!}
+{!! Form::select("role_id",[""=>"Choose Options"]+$roles,null,["class"=>"form-control"]) !!}
+</div>
+    <div class="form-group">
+{!! Form::label("status","Status: ") !!}
+{!! Form::select("Status",array(1 => "Active",0=>"Not Active"),0,["class"=>"form-control"]) !!}
+    </div>
+<div class="form-group">
+    {!! Form::label("password","Password: ") !!}
+    {!! Form::password("password",["class"=>"form-control"]) !!}
+</div>
+    {!! Form::submit("Crea un utente", ["class"=>"btn btn-info"]) !!}
+
+    {!! Form::close() !!}
+
+<div>
+    @include("includes.form_error")
+</div>
+
+@stop
